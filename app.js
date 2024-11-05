62f43a39b22a1f3a853d41b9f85a5901f38d8500
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-11-05 23:07:34 +0500

    complete

--- app.js
+++ app.js
@@ -1,102 +1,131 @@
-// Assignmetn no 17 
+// Assignmetn no 17
 // | FUNCTIONS| SWITCH | WHILE.. DO-WHILE  |
 
 // Question no 1
-// Write a custom function power ( a, b ), to calculate the value of a raised to b.
 function power(a, b) {
-    var result = 1;
-    for (var i = 0; i < b; i++) {
-        result *= a;
-    }
-    return result;
+  var result = 1;
+  for (var i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;
 }
 console.log(power(2, 3));
 
 // Question no 2
-// Any year is entered through the keyboard. Write a function to determine whether the year is a leap year or not. Leap years ..., 2000, 2012, 2016, etc.
 function isLeapYear(year) {
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 console.log(isLeapYear(2020));
 // true
 console.log(isLeapYear(2021));
 
 // Question no 3
-// If the lengths of the sides of a triangle are denoted by a, b, and c, then area of a triangle is given by area = (s(s - a)(s - b)(s - c)) where, s = ( a + b + c ) / 2
 function calculateTriangleArea(a, b, c) {
-    var s = (a + b + c) / 2;
-    var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-    return area;
+  var s = (a + b + c) / 2;
+  var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  return area;
 }
 console.log(calculateTriangleArea(3, 4, 5));
 
 // Question no 4
-// Write a function that receives marks received by a student in 3 subjects and returns the average and percentage of these marks. there should be 3 functions one is the mainFunction and other are sub functions.
 function calculateAverageAndPercentage(marks) {
-    var totalMarks = 0;
-    for (var i = 0; i < marks.length; i++) {
-        totalMarks += marks[i];
-    }
-    var average = totalMarks / marks.length;
-    var percentage = (totalMarks / (marks.length * 100)) * 100;
-    return {
-        average: average,
-        percentage: percentage
-    };
+  var totalMarks = 0;
+  for (var i = 0; i < marks.length; i++) {
+    totalMarks += marks[i];
+  }
+  var average = totalMarks / marks.length;
+  var percentage = (totalMarks / (marks.length * 100)) * 100;
+  return {
+    average: average,
+    percentage: percentage,
+  };
 }
 function mainFunction() {
-    var marks = [80, 90, 70];
-    var result = calculateAverageAndPercentage(marks);
-    console.log("Average: " + result.average);
-    console.log("Percentage: " + result.percentage + "%");
+  var marks = [80, 90, 70];
+  var result = calculateAverageAndPercentage(marks);
+  console.log("Average: " + result.average);
+  console.log("Percentage: " + result.percentage + "%");
 }
 
 // Question no 5
-//You have learned the function indexOf. Code your own custom function that will perform the same functionality. You can code for single character as of now.
 function customIndexOf(str, char) {
-    for (var i = 0; i < str.length; i++) {
-        if (str[i] === char) {
-            return i;
-        }
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
 console.log(customIndexOf("hello", "l"));
 // Question no 6
-// Write a function to delete all vowels from a sentence. Assume that the sentence is not in uppercase and
-//  also not in lowercase.
 function removeVowels(sentence) {
-    var vowels = ["a", "e", "i", "o", "u"];
-    var result = "";
-    for (var i = 0; i < sentence.length; i++) {
-        var isVowel = false;
-        for (var j = 0; j < vowels.length; j++) {
-            if (sentence[i].toLowerCase() === vowels[j]) {
-                isVowel = true;
-                break;
-            }
-        }
-        if (!isVowel) {
-            result += sentence[i];
-        }
+  var vowels = ["a", "e", "i", "o", "u"];
+  var result = "";
+  for (var i = 0; i < sentence.length; i++) {
+    var isVowel = false;
+    for (var j = 0; j < vowels.length; j++) {
+      if (sentence[i].toLowerCase() === vowels[j]) {
+        isVowel = true;
+        break;
+      }
     }
-    return result;
+    if (!isVowel) {
+      result += sentence[i];
+    }
+  }
+  return result;
 }
 
 // Question no 7
-// Write a function with switch statement to count the number of 1’s that appear in a given string.
 function countOnes(str) {
-    var count = 0;
-    for (var i = 0; i < str.length; i++) {
-        switch (str[i]) {
-            case "1":
-                count++;
-                break;
-        }
+  var count = 0;
+  for (var i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      case "1":
+        count++;
+        break;
     }
-    return count;
+  }
+  return count;
 }
+console.log(countOnes("10101"));
+
+// Question no 8
+function convertDistance(distance) {
+  var meters = distance * 1000;
+  var feet = distance * 3280.84;
+  var inches = distance * 39370.1;
+  var centimeters = distance * 100000;
+  console.log("Distance in meters: " + meters);
+  console.log("Distance in feet: " + feet);
+  console.log("Distance in inches: " + inches);
+  console.log("Distance in centimeters: " + centimeters);
+}
+convertDistance(10);
+
+// Question no 9
+function calculateOvertimePay(hoursWorked) {
+  var overtimeHours = hoursWorked - 40;
+  if (overtimeHours <= 0) {
+    return 0;
+  } else {
+    var overtimePay = overtimeHours * 12;
+    return overtimePay;
+  }
+}
+console.log(calculateOvertimePay(45));
+
+// Question no 10
+function calculateCurrencyNotes(amount) {
+  var notes100 = Math.floor(amount / 100);
+  var notes50 = Math.floor((amount % 100) / 50);
+  var notes10 = Math.floor(((amount % 100) % 50) / 10);
+  console.log("Notes of 100: " + notes100);
+  console.log("Notes of 50: " + notes50);
+  console.log("Notes of 10: " + notes10);
+}
+calculateCurrencyNotes(500);
